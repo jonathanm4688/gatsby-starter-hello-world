f6de3f5601e4e493b35dae0f542123935ed1c603
jonathanm4688 <42524090+jonathanm4688@users.noreply.github.com>
2020-01-23 11:28:19 -0800

    Creating a static query component

--- src/examples/Header-Static.js
+++ src/examples/Header-Static.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import {useStaticQuery, graphql} from 'gatsby'
+
+const getData = graphql`{
+    site {
+      siteMetadata {
+        title,
+          author,
+        description,
+        data{
+          name,
+          age
+        }
+      }
+    }
+  }`
 
 export const HeaderStatic = () => {
     return (
         <div>
-            
+            I am using a static query
         </div>
     )
 }
